3bdb738b346b9c87ababd5ed7922e20c1459d567
Sofie Tønder <sofie.ts@hotmail.com>
2025-01-24 09:56:12 +0100

    installed eslint

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
 	content: ["./**/*.{html,js,ts}", "!./node_modules/**/*"],
 	theme: {
 		extend: {},
+		colors: {
+			beige: "#F6F4F0",
+			red: "#D6482C",
+			customblack: "#161613",
+			brown: "#A58063",
+			gray: "#AFA9A4",
+		},
 	},
 	plugins: [],
 };
