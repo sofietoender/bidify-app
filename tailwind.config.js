a9bf0130aacf9dbe3162bd86509347bb14968e47
Sofie Tønder <sofie.ts@hotmail.com>
2025-01-24 16:37:01 +0100

    updated tailwind file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 module.exports = {
 	content: ["./**/*.{html,js,ts}", "!./node_modules/**/*"],
 	theme: {
-		extend: {},
-		colors: {
-			beige: "#F6F4F0",
-			red: "#D6482C",
-			customblack: "#161613",
-			brown: "#A58063",
-			gray: "#AFA9A4",
-			white: "#FFFFFF",
+		extend: {
+			colors: {
+				beige: "#F6F4F0",
+				red: "#D6482C",
+				customblack: "#161613",
+				brown: "#A58063",
+				gray: "#AFA9A4",
+				white: "#FFFFFF",
+			},
 		},
 	},
 	plugins: [],
