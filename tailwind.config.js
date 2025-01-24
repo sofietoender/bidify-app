4188418fe56dded69483d08664a99c60ce209947
Sofie Tønder <sofie.ts@hotmail.com>
2025-01-24 22:15:05 +0100

    updated tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 module.exports = {
 	content: [
 		"./index.html",
-		"./src/**/*.{html,js,ts}", // Sørger for at all JS i src inkluderes
-		"!./node_modules/**/*",
-		"./**/*.js",
-		"./**/*.mjs",
-		"./**/*.html",
+		"./src/**/*.{html,js,ts}",
+		"./**/*.html", // Ensure all HTML files are included
 	],
 	theme: {
 		extend: {
