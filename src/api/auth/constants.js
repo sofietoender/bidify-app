b2ae0fe99c0b59daf888695b0d5927375af227a7
Sofie Tønder <sofie.ts@hotmail.com>
2025-01-28 11:27:46 +0100

    deleted duplicate file

--- src/api/auth/constants.js
+++ src/api/auth/constants.js
@@ -1,1 +1,28 @@
 export const baseUrl = "https://v2.api.noroff.dev/auction/listings";
+
+// LISTINGS
+// GET all listings (homepage): /auction/listings
+// GET single listing: /auction/listings/<id>
+
+// GET Search listings /auction/listings/search?q=<query>
+
+// POST Create listing: /auction/listings
+
+// POST bid on listing /auction/listings/<id>/bids
+
+// PUT Update listing /auction/listings/<id>
+
+// PROFILE
+// GET profile details: /auction/profiles/<name>
+// GET all wins by profile (profilsiden): /auction/profiles/<name>/wins
+// GET all bids by profile: /auction/profiles/<name>/bids
+
+// POST (legg til relevant info her)
+
+// PUT
+// PUT update profile (oppdaterer banner og avatar): /auction/profiles/<name>
+
+// USER LOGIN
+
+// POST Login a user /auth/login
+// Register a new user profile /auth/register
